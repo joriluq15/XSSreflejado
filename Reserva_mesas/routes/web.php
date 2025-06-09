a7316880da7d58b4401c0607f4ddcd86ab3e1876
Jori <91052709+joriluq15@users.noreply.github.com>
2025-06-09 18:58:26 +0200

    Delete Reserva_mesas directory

--- Reserva_mesas/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReservationController;
-
-Route::get('/', [ReservationController::class, 'index'])->name('reservations.index');
-Route::get('/reservations/create', [ReservationController::class, 'create'])->name('reservations.create');
-Route::post('/reservations', [ReservationController::class, 'store'])->name('reservations.store');
-Route::delete('/reservations/{id}', [ReservationController::class, 'destroy'])->name('reservations.destroy');
